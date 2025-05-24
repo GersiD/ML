b0f047c51cf7fb93ed3248581a315369b49037d3
Gersi.doko <Gersi.doko@unh.edu>
2025-05-24 14:16:32 -0400

    improve wine model by using dropout

--- nn-torch/nn-torch.py
+++ nn-torch/nn-torch.py
@@ -4,25 +4,29 @@ import numpy as np
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 class NN_Torch(nn.Module):
     def __init__(self, xdims, ydims) -> None:
         super(NN_Torch, self).__init__()
         self.linear1 = nn.Linear(xdims, 64)
         self.linear2 = nn.Linear(64, 64)
+        self.dropout = nn.Dropout(0.2)
         self.linear3 = nn.Linear(64, ydims)
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=1e-2)
 
     def forward(self, x):
         x = torch.relu(self.linear1(x))
         x = torch.relu(self.linear2(x))
+        x = self.dropout(x)
         return self.linear3(x)
 
     def loss(self, y_pred, y):
-        return ((y_pred - y) ** 2).mean()
+        return nn.functional.mse_loss(y_pred, y)
 
     def fit(self, X, Y, epochs):
         losses = []
+        self.train()
         for epoch in range(epochs):
             y_pred = self.forward(X)
             l = self.loss(y_pred, Y)
@@ -35,6 +39,7 @@ class NN_Torch(nn.Module):
             self.optimizer.zero_grad()
             if epoch % 10 == 0:
                 print(f'epoch {epoch}: loss = {l}')
+        self.eval()
         return losses
 
 def fit_torch(X, Y, epochs):
@@ -48,7 +53,14 @@ def fit_torch(X, Y, epochs):
     plt.title('Loss vs. Epoch for PyTorch NN')
     plt.savefig('./plots/loss_vs_epoch_torch.pdf')
     plt.show()
-    return losses
+    return model, losses
+
+def assess_torch(X, Y, model):
+    print("Assessing PyTorch NN model")
+    y_pred = model.forward(X)
+    accuracy = nn.functional.mse_loss(y_pred, Y)
+    print(f"Accuracy: {accuracy.item()}")
+
 
 def main():
     torch.manual_seed(8675309)
@@ -57,9 +69,13 @@ def main():
     # print(df[["fixed_acidity", "volatile_acidity", "citric_acid", "residual_sugar", "chlorides", "free_sulfur_dioxide", "total_sulfur_dioxide", "density", "pH", "sulphates", "alcohol"]].head())
     X = df[["fixed_acidity", "volatile_acidity", "citric_acid", "residual_sugar", "chlorides", "free_sulfur_dioxide", "total_sulfur_dioxide", "density", "pH", "sulphates", "alcohol"]]
     y = df[["quality"]]
-    X = torch.tensor(X.values, dtype=torch.float32)
-    Y = torch.tensor(y.values, dtype=torch.float32)
-    torch_losses = fit_torch(X, Y, 100)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.1, random_state=8675309)
+    X_train = torch.tensor(X_train.values, dtype=torch.float32)# pyright: ignore[reportAttributeAccessIssue]
+    X_test = torch.tensor(X_test.values, dtype=torch.float32)# pyright: ignore[reportAttributeAccessIssue]
+    Y_train = torch.tensor(Y_train.values, dtype=torch.float32)# pyright: ignore[reportAttributeAccessIssue]
+    Y_test = torch.tensor(Y_test.values, dtype=torch.float32)# pyright: ignore[reportAttributeAccessIssue]
+    model, losses = fit_torch(X_train, Y_train, epochs=100)
+    assess_torch(X_test, Y_test, model)
 
 if __name__ == "__main__":
     main()
